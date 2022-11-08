067fc58d788f2f5822e0ede88ea2b6755a25adbe
AayushMandavya <70195769+AayushMandavya@users.noreply.github.com>
2022-11-08 10:51:08 +0545

    Add files via upload

--- class4.php
+++ class4.php
@@ -1,9 +1,16 @@
 <?php
 
     class User{
-        public $name='Aayush';
-        private $email='aayushmandavya@gmail.com';
-        public $password='pass';
+        public $name;
+        protected $email;
+        public $password;
+
+        public function __construct($name,$email,$password){
+
+                $this->name=$name;
+                $this->email=$email;
+                $this->password=$password;
+        }
 
         public function login(){
             echo $this->name . ' has logged in!';
@@ -16,21 +23,29 @@
         }
     }
     class Admin extends User{
+            public $role='superman Admin';
 
+            public function __construct($name,$email,$password,$role){
+                parent ::__construct($name,$email,$password);
+                $this->role=$role;
+        }
+            // public getEmail(){
+            //     echo '<br>User Logged in'. $this->email;
+            // }
     }
     
-     $user= new User();
-     $user -> name='mandu';
+     $user= new User('aayush12','a@mail.com','pass12');
+    //  $user -> name='mandu';
      $user->login();
      echo "<br>";
    
 
-     $user->setEmail('newestmail@g.com');
-   $admin = new Admin();
-   $admin -> name='Admin';
+//      $user->setEmail('newestmail@g.com');
+   $admin = new Admin('admin','admin@mail.com','admin11');
+//    $admin -> name='Admin';
    $admin-> login();
    echo '<br>';
 
-   $admin->setEmail('newadmin@gmail.com');
+//    $admin->setEmail('newadmin@gmail.com');
 
 ?>
